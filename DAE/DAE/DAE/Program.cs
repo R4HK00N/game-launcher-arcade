d48994164c03230e55f58f12c0a05200b9a15210
NickSwitchSchool <97087180@st.deltion.nl>
2024-03-04 16:48:22 +0100

    Emulator start
    -Emulator can now detect keystrokes while running on the background

---
+++ DAE/DAE/DAE/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Timers;
+
+namespace KeyLogger
+{
+    class Program
+    {
+        private String path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Log.txt";
+
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+        static void Main(string[] args)
+        {
+            new Program().start();
+        }
+
+        private void start()
+        {
+            Console.WriteLine("================================\r\n   ______   _______  _______ \r\n  |      | |   _   ||       |\r\n  |  _    ||  |_|  ||    ___|\r\n  | | |   ||       ||   |___ \r\n  | |_|   ||       ||    ___|\r\n  |       ||   _   ||   |___ \r\n  |______| |__| |__||_______|\r\n\r\n================================\r\nEmulator for the Deltion Arcade.\r\nMade by Nick Schakelaar\r\n================================");
+
+            while (true)
+            {
+                //R becomes W
+                if (GetAsyncKeyState(82) == 32768)
+                {
+                    Console.WriteLine("Simulating W press");
+                }
+                //D becomes A
+                else if (GetAsyncKeyState(68) == 32768)
+                {
+                    Console.WriteLine("Simulating A press");
+                }
+            }
+        }
+    }
+}
